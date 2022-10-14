8b05e6addedfff27faa7a7636f34df6bfce8d7fe
Arthur Chevalier <arthur.a.chevalier@gmail.com>
2022-10-14 14:55:08 +0200

    Ajout commentaire de classe

--- Assets/Scripts/AbstractBoard/Unit.cs
+++ Assets/Scripts/AbstractBoard/Unit.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public abstract class Unit 
 {
+    //health is 100 points
+    //maxUnit is the initial manpower of the unit
+    //must be updated with health points 
     private int health, maxUnit, morale;
 }
